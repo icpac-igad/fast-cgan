b29806b36eed4a5215100ee6c5ae9226db0174f4
Jason <jaysnmury@gmail.com>
2024-12-18 09:05:38 +0300

    added GPU setup config

---
+++ cloud/training-gpu-server.tf
@@ -0,0 +1,94 @@
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "sewaa-gpu-training-server" {
+  attached_disk {
+    device_name = "cgan-training-data"
+    mode        = "READ_WRITE"
+  }
+
+  boot_disk {
+    auto_delete = true
+    device_name = "sewaa-gpu-training-server"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20241210"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  guest_accelerator {
+    count = 1
+    type  = "projects/sewaa-416306/zones/europe-west4-c/acceleratorTypes/nvidia-tesla-t4"
+  }
+
+  labels = {
+    goog-ec-src           = "vm_add-tf"
+    goog-ops-agent-policy = "v2-x86-template-1-3-0"
+  }
+
+  machine_type = "n1-standard-2"
+
+  metadata = {
+    enable-osconfig = "TRUE"
+  }
+
+  name = "sewaa-gpu-training-server"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/sewaa-416306/regions/europe-west4/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "TERMINATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "8979869085-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["http-server", "https-server"]
+  zone = "europe-west4-c"
+}
+
+module "ops_agent_policy" {
+  source        = "github.com/terraform-google-modules/terraform-google-cloud-operations/modules/ops-agent-policy"
+  project       = "sewaa-416306"
+  zone          = "europe-west4-c"
+  assignment_id = "goog-ops-agent-v2-x86-template-1-3-0-europe-west4-c"
+  agents_rule = {
+    package_state = "installed"
+    version       = "latest"
+  }
+  instance_filter = {
+    all = false
+    inclusion_labels = [{
+      labels = {
+        goog-ops-agent-policy = "v2-x86-template-1-3-0"
+      }
+    }]
+  }
+}
